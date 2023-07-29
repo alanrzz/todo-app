8d12f9f680e5c9eaad096b2f91bd575c32c56f43
Alan Rodriguez <alanrodriguezalexandro@gmail.com>
2023-07-28 21:19:03 -0300

    borrado

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,10 +1,18 @@
 import "./Task.css"
 
-const Task = (props) => {
+const Task = ({id,msg,deleteTask,editTask}) => {
+
+  const handleEdit = () => {
+    editTask(id);
+  }
+  
   return (
     <div className="task">
-      <p className="text">{props.msg}</p>
-      <button className="delete-btn" type="button" onClick={(e) => props.tasks(props.msg)}>Eliminar</button>
+      <p className="text">{msg}</p>
+      <div>
+        <button className="edit-btn" type="button" onClick={handleEdit}>Editar</button>
+        <button className="delete-btn" type="button" onClick={() => deleteTask(id)}>Borrar</button>
+      </div>
     </div>
   )
 }
--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,22 +2,25 @@ import { useState } from "react"
 import Task from "../Task"
 import "./Todo.css"
 
+let count = 0;
+
 const Todo = () => {
   const [message, setMessage] = useState("");
   const [tasks, setTasks] = useState([]);
 
   const addTask = () => {
     if (message != "") {
-      setTasks(task => [...task, {msg:message}]);
+      setTasks(task => [...task, {id:count++, msg:message}]);
       setMessage("");
     }
   }
 
-  const deleteTask = (test) => {
-    console.log("eliminar")
-    setTasks([])
-  }
+  const deleteTask = id => setTasks(tasks => tasks.filter(task => task.id !== id));
 
+  const editTask = (id) => setMessage(tasks.find(task => task.id === id).msg);
+  
+  const func = {deleteTask,editTask};
+  
   return (
     <div className="card">
       <div className="header-card">
@@ -28,7 +31,7 @@ const Todo = () => {
         value={message} />
         <button className="add-btn" type="button" onClick={addTask}>Agregar</button>
       </div>
-      {tasks.map((task, index) => <Task key={index} msg={task.msg} tasks={deleteTask}/>)}
+      {tasks.map(task => <Task key={task.id} {...task} {...func}/>)}
     </div>
   )
 }

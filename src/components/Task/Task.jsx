af965a0c4150ec8008ddfdb57aa1422a73c869d2
Alan Rodriguez <alanrodriguezalexandro@gmail.com>
2023-07-29 12:13:50 -0300

    completo edicion

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,16 +1,11 @@
 import "./Task.css"
 
 const Task = ({id,msg,deleteTask,editTask}) => {
-
-  const handleEdit = () => {
-    editTask(id);
-  }
-  
   return (
     <div className="task">
       <p className="text">{msg}</p>
       <div>
-        <button className="edit-btn" type="button" onClick={handleEdit}>Editar</button>
+        <button className="edit-btn" type="button" onClick={() => editTask(id)}>Editar</button>
         <button className="delete-btn" type="button" onClick={() => deleteTask(id)}>Borrar</button>
       </div>
     </div>
--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -7,19 +7,39 @@ let count = 0;
 const Todo = () => {
   const [message, setMessage] = useState("");
   const [tasks, setTasks] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
+  const [item, setItem] = useState(null);
 
   const addTask = () => {
     if (message != "") {
-      setTasks(task => [...task, {id:count++, msg:message}]);
+      setTasks(tasks => [...tasks, {id:count++, msg:message}]);
       setMessage("");
+    }
+  }
+
+  const saveTask = () => {
+    if (message != "") {
+      setTasks(tasks => tasks.map(task => {
+        if (task === item) {
+          task.msg = message;
+        }
+        return task;
+      }))
+    setIsEdit(false);
+    setMessage("");
     }
   }
 
   const deleteTask = id => setTasks(tasks => tasks.filter(task => task.id !== id));
 
-  const editTask = (id) => setMessage(tasks.find(task => task.id === id).msg);
-  
-  const func = {deleteTask,editTask};
+  const editTask = (id) => {
+    const item = tasks.find(task => task.id === id);
+    setItem(item);
+    setMessage(item.msg);
+    setIsEdit(true);
+  };
+
+  const funcs = {deleteTask,editTask};
   
   return (
     <div className="card">
@@ -29,9 +49,13 @@ const Todo = () => {
         placeholder="Ingrese una tarea" 
         onChange={e => setMessage(e.target.value)}
         value={message} />
-        <button className="add-btn" type="button" onClick={addTask}>Agregar</button>
+        {isEdit ? 
+          <button className="save-btn" type="button" onClick={saveTask}>Guardar</button> 
+          :
+          <button className="add-btn" type="button" onClick={addTask}>Agregar</button>
+        }
       </div>
-      {tasks.map(task => <Task key={task.id} {...task} {...func}/>)}
+      {tasks.map(task => <Task key={task.id} {...task} {...funcs}/>)}
     </div>
   )
 }

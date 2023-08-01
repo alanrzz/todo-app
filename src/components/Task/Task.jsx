6a9de7915666e1be11500ea102a3911cb4365d75
Alan Rodriguez <alanrodriguezalexandro@gmail.com>
2023-08-01 12:59:07 -0300

    add icons

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,4 +1,6 @@
-import './Task.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import './Task.css';
 
 const Task = (props) => {
   const { id, msg, deleteTask, editTask } = props;
@@ -6,8 +8,12 @@ const Task = (props) => {
     <div className="task">
       <p className="text">{msg}</p>
       <div>
-        <button className="edit-btn" type="button" onClick={() => editTask(id)}>Editar</button>
-        <button className="delete-btn" type="button" onClick={() => deleteTask(id)}>Borrar</button>
+        <button className="edit-btn" type="button" onClick={() => editTask(id)}>
+          <FontAwesomeIcon icon={faPenToSquare} />
+        </button>
+        <button className="delete-btn" type="button" onClick={() => deleteTask(id)}>
+          <FontAwesomeIcon icon={faTrashCan}/>
+        </button>
       </div>
     </div>
   )
--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,11 +1,11 @@
-import { useRef, useState } from 'react'
-import Task from '../Task'
-import './Todo.css'
+import { useRef, useState } from 'react';
+import Task from '../Task';
+import './Todo.css';
 
 const Todo = () => {
-  const [task, setTask] = useState({msg:""})
-  const [tasks, setTasks] = useState([])
-  const [editingTask, setEditingTask] = useState(false)
+  const [task, setTask] = useState({msg:""});
+  const [tasks, setTasks] = useState([]);
+  const [editingTask, setEditingTask] = useState(false);
   const inputRef = useRef();
 
   const validation = e => {
@@ -16,15 +16,15 @@ const Todo = () => {
   const addTask = e => {
     if (validation(e)) {
       setTasks([...tasks, {id: (tasks[tasks.length - 1]?.id || 0) + 1, msg: task?.msg}]);
-      setTask({msg:''})
+      setTask({msg:''});
     }
   }
 
   const saveTask = e => {
     if (validation(e)) {
-      setTasks(prevTasks => prevTasks.map(prevTask => (prevTask.id === task.id ? {...prevTask, msg: task.msg} : prevTask)))
+      setTasks(prevTasks => prevTasks.map(prevTask => (prevTask.id === task.id ? {...prevTask, msg: task.msg} : prevTask)));
       setEditingTask(false);
-      setTask({msg:''})
+      setTask({msg:''});
     }
   }
 

70b8730447b9a5314aa94b6810f4f7d40b7d0a85
Alan Rodriguez <alanrodriguezalexandro@gmail.com>
2023-08-01 14:43:07 -0300

    add checkbox

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,13 +1,17 @@
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashCan, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 import './Task.css';
 
 const Task = (props) => {
   const { id, msg, deleteTask, editTask } = props;
+  const [checked, setChecked] = useState(false);
+
   return (
     <div className="task">
-      <p className="text">{msg}</p>
-      <div>
+      <p className={checked ? "checked-text" : "text"}>{msg}</p>
+      <div className="task-right-section">
+        <input className="complete-task-checkbox" type="checkbox" onClick={e => setChecked(e.target.checked)} />
         <button className="edit-btn" type="button" onClick={() => editTask(id)}>
           <FontAwesomeIcon icon={faPenToSquare} />
         </button>
